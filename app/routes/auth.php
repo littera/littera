abac395b0abe747e0db3843fd51e1ddab33a9af2
Nikolay Denisenko <mail2nikolas@gmail.com>
2014-07-18 19:11:02 +0400

    minifix auth view

--- app/routes/auth.php
+++ app/routes/auth.php
@@ -5,8 +5,8 @@ Route::group(['prefix'=>'auth'], function()
 	Route::get('logout', ['before'=>'auth','uses'=>'AuthController@getLogout']);
 	Route::group(['before'=>'guest'], function ()
 	{
-		Route::get('login', 'AuthController@getLogin');
-		Route::post('login', 'AuthController@postLogin');
+		Route::get('login', ['as' => 'auth.getLogin', 'uses' => 'AuthController@getLogin']);
+		Route::post('login', ['as' => 'auth.postLogin', 'uses' => 'AuthController@postLogin']);
 		Route::get('register', 'AuthController@getRegister');
 		Route::post('register', 'AuthController@postRegister');
 		Route::get('activate/{token}', 'AuthController@getActivate');
--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.auth')
 
 @section('container')
-<form class="form-login" role="form">
+{{ Form::open(['route' => 'auth.postLogin', 'class' => 'form-login', 'role' => 'form']) }}
 	<h2 class="form-login-heading text-center"><i class="fa fa-lock"></i> Littera</h2>
-	<input type="email" class="form-control" placeholder="{{ trans('auth/views.input_login') }}" required autofocus>
-	<input type="password" class="form-control" placeholder="{{ trans('auth/views.input_password') }}" required>
+	{{ Form::text('login', null, ['class' => 'form-control', 'placeholder' => trans('auth/views.input_login'), 'require', 'autofocus']) }}
+	{{ Form::password('password', ['class' => 'form-control', 'placeholder' => trans('auth/views.input_password'), 'require']) }}
 	<div class="checkbox">
 		<label>
 			<input type="checkbox" value="remember-me"> {{ trans('auth/views.input_remember_me') }}
 		</label>
 	</div>
 	<button class="btn btn-lg btn-primary btn-block" type="submit">{{ trans('auth/views.button_login') }}</button>
-</form>
+{{ Form::close() }}
 @stop
