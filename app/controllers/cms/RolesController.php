898b906ce6f8fa3c2e7417b6738b106df504dd2e
Nikolay Denisenko <mail2nikolas@gmail.com>
2014-08-27 15:01:57 +0400

    roles & permissions controllers

---
+++ app/controllers/cms/RolesController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Cms;
+
+class RolesController extends CmsController
+{
+	public function getIndex()
+	{
+		//
+	}
+
+	public function getCreate()
+	{
+		//
+	}
+
+	public function postCreate()
+	{
+		//
+	}
+
+	public function getEdit($id)
+	{
+		//
+	}
+
+	public function postEdit($id)
+	{
+		//
+	}
+
+	public function getDelete($id)
+	{
+		//
+	}
+
+	public function postDelete($id)
+	{
+		//
+	}
+
+	public function getRestore($id)
+	{
+		//
+	}
+
+	public function postRestore($id)
+	{
+		//
+	}
+
+}
